79b85991d940ced6dbbb5c8cca9f2f51a3e978fa
LibertySky <hello@LibertySkygraphics.com>
2021-01-20 08:13:08 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 const App = () => {
+	const [tasks, setTasks] = useState([
+		{
+			id: 1,
+			text: 'Learn Vue',
+			day: 'Jan 21 at 9:00am',
+			reminder: false,
+		},
+		{
+			id: 2,
+			text: 'Visit dentist',
+			day: 'Jan 20 at 12:00am',
+			reminder: true,
+		},
+	]);
 	return (
 		<div className='container'>
 			<Header  />
+			<Tasks tasks={tasks}/>
 		</div>
 	);
 };
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,12 @@
-const Tasks = () => {
-	return <div></div>;
+import Task from './Task';
+const Tasks = ({ tasks }) => {
+	return (
+		<>
+			{tasks.map((task) => (
+				<Task key={task.id} task={task} />
+			))}
+		</>
+	);
 };
 
 export default Tasks;
