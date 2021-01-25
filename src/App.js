6c9d5a3f1b7f37f88b3941f7783b20847218ce7c
LibertySky <hello@LibertySkygraphics.com>
2021-01-25 11:44:22 -0400

    POST data

--- src/App.js
+++ src/App.js
@@ -23,14 +23,28 @@ const App = () => {
 	};
 
 	// add task
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
-		const newTask = { id, ...task };
-		setTasks([...tasks, newTask]);
+	const addTask = async (task) => {
+		const res = await fetch('http://localhost:5000/tasks', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(task),
+		});
+		const data = await res.json();
+		setTasks([...tasks, data]);
+
+		// create id for the task
+		// const id = Math.floor(Math.random() * 10000) + 1;
+		// const newTask = { id, ...task };
+		// setTasks([...tasks, newTask]);
 	};
 
 	// delete task
-	const deleteTask = (id) => {
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE',
+		});
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
