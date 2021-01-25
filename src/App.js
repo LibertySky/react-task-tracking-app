489d9a578bc8a872d9240956fe5e87ac4cc25cb1
LibertySky <hello@LibertySkygraphics.com>
2021-01-25 11:30:13 -0400

    fetch data from back-end

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 
 const App = () => {
-	const [showAddTask, setShowAddTask]=useState(false)
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			text: 'Learn Vue',
-			day: 'Jan 21 at 9:00am',
-			reminder: false,
-		},
-		{
-			id: 2,
-			text: 'Visit dentist',
-			day: 'Jan 20 at 12:00am',
-			reminder: true,
-		},
-	]);
+	const [showAddTask, setShowAddTask] = useState(false);
+	const [tasks, setTasks] = useState([]);
+
+	// fetch data
+	useEffect(() => {
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks();
+			setTasks(tasksFromServer);
+		};
+		getTasks();
+	}, []);
+
+	const fetchTasks = async () => {
+		const response = await fetch('http://localhost:5000/tasks');
+		const data = await response.json();
+		return data;
+	};
 
 	// add task
-	const addTask=(task)=>{
-		const id =Math.floor(Math.random()*10000)+1
-		const newTask={id,...task}
-		setTasks([...tasks,newTask])
-	}
+	const addTask = (task) => {
+		const id = Math.floor(Math.random() * 10000) + 1;
+		const newTask = { id, ...task };
+		setTasks([...tasks, newTask]);
+	};
 
 	// delete task
 	const deleteTask = (id) => {
@@ -43,8 +45,11 @@ const App = () => {
 
 	return (
 		<div className='container'>
-			<Header onAdd={()=>setShowAddTask(!showAddTask)} showAddBtn={showAddTask}/>
-			{showAddTask && <AddTask onAdd={addTask}/>}
+			<Header
+				onAdd={() => setShowAddTask(!showAddTask)}
+				showAddBtn={showAddTask}
+			/>
+			{showAddTask && <AddTask onAdd={addTask} />}
 			{tasks.length > 0 ? (
 				<Tasks tasks={tasks} onDelete={deleteTask} onToggle={reminder} />
 			) : (
